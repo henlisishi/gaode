cf6807bcdbc92f19cf29d234e722aa0f86243a73
taorl <taorunlong3721@dingtalk.com>
2019-06-03 11:25:28 +0800

    m

---
+++ MAMapKit_2D_Demo/Configuration/APIKey.h
@@ -0,0 +1,17 @@
+//
+//  APIKey.h
+//  SearchV3Demo
+//
+//  Created by songjian on 13-8-14.
+//  Copyright (c) 2013年 songjian. All rights reserved.
+//
+
+#ifndef SearchV3Demo_APIKey_h
+#define SearchV3Demo_APIKey_h
+
+/* 使用高德地图API，请注册Key，注册地址：http://lbs.amap.com/dev/#/ */
+
+const static NSString *APIKey = @"da5f5bd8b33f537ca59424807b4ed62e";
+const static NSString *TableID = @"";
+
+#endif
